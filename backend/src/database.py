997e3d0daf643daf6c7e37cadfb281715c813114
Rita Efremova <rita@MacBook-Pro.local>
2025-01-17 16:53:03 +0100

    data

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,20 +1,12 @@
-import csv
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"
 
-
-# Считываем DATABASE_URL из переменных окружения
-SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require")
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
