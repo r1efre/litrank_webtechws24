35435703693d40fae3c8f093441827a573e2b04e
Rita Efremova <rita@MacBook-Pro.local>
2025-01-17 14:57:56 +0100

    database

--- backend/src/database.py
+++ backend/src/database.py
@@ -6,13 +6,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# Используем переменную окружения, если она есть
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require")
 
-# Преобразование старого формата postgres:// в postgresql:// для SQLAlchemy
-if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"  # Или другой URL для PostgreSQL/MySQL
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
