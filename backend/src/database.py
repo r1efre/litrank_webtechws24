a63d6d716680032a5060d70bd29ab9d36f71d587
Rita Efremova <rita@MacBook-Pro.local>
2025-01-17 16:47:58 +0100

    oki

--- backend/src/database.py
+++ backend/src/database.py
@@ -9,9 +9,13 @@ from sqlalchemy.orm import sessionmaker
 
 
 # Считываем DATABASE_URL из переменных окружения
-SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"
+SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
 
