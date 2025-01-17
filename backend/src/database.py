17420f2357a06917de3a51fd0393f73d9d11c4b2
Rita Efremova <rita@MacBook-Pro.local>
2025-01-17 16:43:14 +0100

    database

--- backend/src/database.py
+++ backend/src/database.py
@@ -12,15 +12,6 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-from sqlalchemy import create_engine
-
-SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
-
 
 
 
