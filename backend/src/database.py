686f931e63fe56be3e77e7482b02ffc6e3e6de67
Rita Efremova <rita@MacBook-Pro.local>
2025-01-17 16:35:34 +0100

    database

--- backend/src/database.py
+++ backend/src/database.py
@@ -9,15 +9,18 @@ from sqlalchemy.orm import sessionmaker
 
 
 # Считываем DATABASE_URL из переменных окружения
-SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require")
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"
 
-if not SQLALCHEMY_DATABASE_URL:
-    raise ValueError("No DATABASE_URL environment variable found")
-
-# Создаём движок с SSL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+from sqlalchemy import create_engine
+
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_VhEyCyHPV6jdhs65GqL@pg-1a34ffe7-worker-3c3d.g.aivencloud.com:21815/defaultdb?sslmode=require"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
+
 
 
 
